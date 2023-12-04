bad1c0860d777b8feb672b374d1e2eeaeecd445f
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-04 18:50:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TryCatchFinally_Hvatanje_Gresaka
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] karakteri = { 'a', 'b', 'c', };
+            try
+            {
+                Console.WriteLine(karakteri[1]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Doslo je do greske (ovo je poruka koju ja ostavljam)");
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("ovo ce raditi sta god da se desi radilo ili ne ovo gore"); 
+            }
+        }
+    }
+} 
