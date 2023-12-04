14f7c936bff73604439d01b2d34bd1f0bf8c125a
djavo666aca <52077531+djavo666aca@users.noreply.github.com>
2023-12-04 21:16:28 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,34 @@
-﻿using System;
-
-namespace TryCatchFinally_Hvatanje_Gresaka
-
+﻿namespace Error_handling__Try_Catch_Finally__Rukovanje_greskama
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            char[] karakteri = { 'a', 'b', 'c', };
-            try
+            Console.WriteLine("Upisi neki broj");
+            string ubacibr1 = Console.ReadLine();
+
+            try 
             {
-                Console.WriteLine(karakteri[1]);
+                int br1 = int.Parse(ubacibr1);
+                Console.WriteLine($"Pomnozi br: {br1 * br1}");
             }
-            catch(Exception e)
+            catch (FormatException)
             {
-                Console.WriteLine("Doslo je do greske (ovo je poruka koju ja ostavljam)");
-                Console.WriteLine(e.Message);
+                Console.WriteLine("FormatException: ubaci validan br");
             }
-            finally {
-                Console.WriteLine("ovo ce raditi sta god da se desi radilo ili ne ovo gore"); 
+            catch (OverflowException) 
+            {
+            Console.WriteLine("OverflowException: broj je premali ili preveliki za int");
             }
+            catch(Exception) 
+            {
+            Console.WriteLine("Doslo je do nepoznate gdeske: Molim te kontaktiraj me preko gmail.com");
+            }
+            finally 
+            {
+                Console.WriteLine("finally se izvrsava svakako bilo gresaka ili ne");
+            }
+            Console.ReadKey();
         }
     }
-} 
+}
